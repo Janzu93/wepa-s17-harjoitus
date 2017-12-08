09cc9ee2422e3de24ee0e1bfb7da3cd922d55aad
Janzu93 <janne.lofhjelm@gmail.com>
2017-12-08 15:48:35 +0200

    Index template Placeholder

--- src/main/java/wepa/controller/DefaultController.java
+++ src/main/java/wepa/controller/DefaultController.java
@@ -2,14 +2,12 @@ package wepa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DefaultController {
 
     @GetMapping("*")
-    @ResponseBody
     public String index() {
-        return "Hello World!";
+        return "index";
     }
 }

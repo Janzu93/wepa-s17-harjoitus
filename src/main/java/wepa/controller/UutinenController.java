fbcce3d09601692bfc90e603751112bb2826bb20
Janzu93 <janne.lofhjelm@gmail.com>
2017-12-10 16:46:09 +0200

    Etusivulle 5 tulostus

--- src/main/java/wepa/controller/UutinenController.java
+++ src/main/java/wepa/controller/UutinenController.java
@@ -1,17 +1,21 @@
 package wepa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import wepa.repository.UutinenRepository;
 import wepa.service.FileObjectService;
 import wepa.service.UutinenService;
 
-import javax.transaction.Transactional;
 import java.io.IOException;
 
 @Controller
@@ -21,8 +25,17 @@ public class UutinenController {
     private UutinenService uutinenService;
 
     @Autowired
+    private UutinenRepository uutinenRepository;
+
+    @Autowired
     private FileObjectService fileObjectService;
 
+    @GetMapping("/")
+    public String etusivu(Model model) {
+        Pageable pageable = PageRequest.of(0,5, Sort.Direction.DESC,"julkaisupaiva");
+        model.addAttribute("uutiset", uutinenRepository.findAll(pageable));
+        return "index";
+    }
     @GetMapping("/uutiset/uusi")
     public String luontiSivu() {
         return "uutinen/luoUutinen";
